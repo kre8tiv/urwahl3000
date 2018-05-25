d1de34f3a0c0cf515bf10378f64f593265a96f1a
Lars Schellhas <lars.schellhas@outlook.com>
2018-05-25 17:44:27 +0200

    Alternatives Logo durch Childtheme
    Durch die Verwendung von `get_stylesheet_directory_uri()` anstelle von `get_template_directory_uri()` kann das im Childtheme hinterlegte Logo überschrieben werden und somit eine z.B. lokalisierte Logo-Variante verwendet werden.

--- header.php
+++ header.php
@@ -98,7 +98,7 @@
 			<?php } ?>
 
 				<?php if ( display_header_text() ) : ?>
-					<p id="logo"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="Zur Startseite"><img src="<?php echo get_template_directory_uri(); ?>/lib/images/logo.png" width="185" height="100" alt="<?php bloginfo('name'); ?>"></a></p>
+					<p id="logo"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="Zur Startseite"><img src="<?php echo get_stylesheet_directory_uri(); ?>/lib/images/logo.png" width="185" height="100" alt="<?php bloginfo('name'); ?>"></a></p>
 					
 					<div class="hgroup">
 						<h1 id="site-title"><span><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></span></h1>
