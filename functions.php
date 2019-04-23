fb8c940a7646132aa50c8dc47f821d85d1904043
modulbuero <cj@modulbuero.de>
2019-04-23 12:54:24 +0200

    v2.3.8
    adds a check for required php-version for kal3000 and conditional checks for theme-function-files.

--- functions.php
+++ functions.php
@@ -7,21 +7,32 @@ URL: htp://kre8tiv.de
 
 if ( ! isset( $content_width ) ) $content_width = 783;	
 
-require_once('functions/theme-functions.php');
-require_once('functions/theme-sidebars.php');
-require_once('functions/theme-comments.php');
-require_once('functions/theme-shortcodes.php');
-require_once('functions/admin-dashboard.php');
-require_once('functions/attachment-copyright.php');
-if(apply_filters('urwahl3000_kal3000', true)) {
-	require_once('functions/kal3000/kal3000.php');
+if(file_exists( get_template_directory() . '/functions/theme-functions.php'))
+	require_once( get_template_directory() . '/functions/theme-functions.php');
+if(file_exists( get_template_directory() . '/functions/theme-sidebars.php'))
+	require_once( get_template_directory() . '/functions/theme-sidebars.php');
+if(file_exists( get_template_directory() . '/functions/theme-comments.php'))
+	require_once( get_template_directory() . '/functions/theme-comments.php');
+if(file_exists( get_template_directory() . '/functions/theme-shortcodes.php'))
+	require_once( get_template_directory() . '/functions/theme-shortcodes.php');
+if(file_exists( get_template_directory() . '/functions/theme-dashboard.php'))
+	require_once( get_template_directory() . '/functions/theme-dashboard.php');
+if(file_exists( get_template_directory() . '/functions/attachment-copyright.php'))
+	require_once( get_template_directory() . '/functions/attachment-copyright.php');
+if (version_compare(PHP_VERSION, '5.6.0') >= 0) {
+	if(apply_filters('urwahl3000_kal3000', true)) {
+		require_once('functions/kal3000/kal3000.php');
+	}
 }
 
-require 'functions/theme-update.php';
-$MyThemeUpdateChecker = new ThemeUpdateChecker(
-	'urwahl3000', //Theme slug. Usually the same as the name of its directory.
-	'http://themes.kre8tiv.de/?action=get_metadata&slug=urwahl3000' //Metadata URL.
-);
+if(file_exists( get_template_directory() . '/functions/theme-update.php')) {
+	require_once( get_template_directory() . '/functions/theme-update.php');
+
+	$MyThemeUpdateChecker = new ThemeUpdateChecker(
+		'urwahl3000',
+		'http://themes.kre8tiv.de/?action=get_metadata&slug=urwahl3000'
+	);
+}
 
 function urwahl3000_after_update() {
 	$current_version = wp_get_theme()->get('Version');
