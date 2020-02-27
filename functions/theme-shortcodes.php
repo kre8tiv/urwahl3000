e6c331aca43be393663acfde038cd5d85bfa4fca
modulbuero <cj@modulbuero.de>
2020-02-27 08:29:10 +0100

    v2.6.7
    Adds fontawesome-shortcode – thanks to Thorsten Duhn

--- functions/theme-shortcodes.php
+++ functions/theme-shortcodes.php
@@ -10,9 +10,9 @@ function kr8_sitemap($atts) {
 	$the_id = get_the_ID();
 	
 	$smargs = array(
-	'child_of'     => $the_id,
-	'title_li'     => '',
-	'parent'       => $the_id,
+	'child_of'	 => $the_id,
+	'title_li'	 => '',
+	'parent'	   => $the_id,
 	'sort_order'	=> 'ASC',
 	'sort_column'	=> 'menu_order'
 	);
@@ -33,10 +33,7 @@ function kr8_sitemap($atts) {
 }
 add_shortcode('unterseiten', 'kr8_sitemap');
 
-
-//Excerpt for Pages
 add_post_type_support( 'page', 'excerpt' );
-
 
 //Tabs for Editor ***************
 function tabs_shortcode( $atts, $content = null ) {
@@ -50,18 +47,13 @@ add_shortcode( 'tabs', 'tabs_shortcode' );
 
 function tab_shortcode( $atts, $content = null ) {
 	extract(shortcode_atts(array(
-      'title' => 'Titel anpassen',
+	  'title' => 'Titel anpassen',
    ), $atts));
 	return '<h2>' .$title .'</h2><div>' . $content . '</div>';
 }
 add_shortcode( 'tab', 'tab_shortcode' ); 
 
-
-
-add_filter("the_content", "the_content_filter");
-
 function the_content_filter($content) {
-
 	// array of custom shortcodes requiring the fix 
 	$block = join("|",array("col","tabs","tab"));
 
@@ -72,37 +64,32 @@ function the_content_filter($content) {
 	$rep = preg_replace("/(<p>)?\[\/($block)](<\/p>|<br \/>)?/","[/$2]",$rep);
 
 	return $rep;
-
 }
-
-
-
-
+add_filter("the_content", "the_content_filter");
 
 //Tabs for Editor ***************
 function kr8_pers_listing( ) {
 	
-					    ob_start();
-					 
-					    // define attributes and their defaults
-					    extract( shortcode_atts( array (
-					        'order' => 'date',
-					        'orderby' => 'title',
-					        'posts' => -1,
-					        'category' => '',
-					    ), $atts ) );
-							
-							query_posts('post_type=person&posts_per_page=-1'); 
-							?>
-					    		
-    		
-    					<?php while ( have_posts() ) : the_post(); ?>
-					    	<?php get_template_part( 'content', get_post_type() ); ?>
-					    <?php endwhile; 			
+	ob_start();
+ 
+	// define attributes and their defaults
+	extract( shortcode_atts( array (
+		'order' => 'date',
+		'orderby' => 'title',
+		'posts' => -1,
+		'category' => '',
+	), $atts ) );
+		
+		query_posts('post_type=person&posts_per_page=-1'); 
+		?>
+			
+
+	<?php while ( have_posts() ) : the_post(); ?>
+		<?php get_template_part( 'content', get_post_type() ); ?>
+	<?php endwhile; 			
 		
 }
 //add_shortcode( 'personen', 'kr8_pers_listing' );
-
 
 function myLoop($atts, $content = null) {
 	extract(shortcode_atts(array(
@@ -134,5 +121,67 @@ function myLoop($atts, $content = null) {
 }
 add_shortcode("personen", "myLoop");
 
+/**
+ * Font Awesome Shortcode
+ *
+ * [fa icon="heart" set="fas" margin="r2" color="#000000" class="fa-fw"]
+ *
+ * Icon is set to "heart" by default.
+ * Set is set to "fas" by default.
+ * Margin is set if required. Possible values are:
+ *	 a (all), t (top), b (bottom), l (left), r (right), x (left + right), y (top + bottom)
+ *	 with digit from 0 to 5 (like spacing classes in Bootstrap 4)
+ * Class can be any additional CSS classes incl. all from Font Awesome like size, fixed width etc.
+ * Color is only set as hexadecimal value if required.
+ *
+ * @see <https://fontawesome.com/icons?d=gallery&m=free>
+ * @see <https://fontawesome.com/how-to-use/on-the-web/referencing-icons/basic-use>
+ * @see <https://wordpress.org/plugins/shortcode-for-font-awesome/>
+ */
+function kr8_fontawesome_shortcode($atts) {
+	// @see <https://torquemag.io/2017/06/custom-shortcode/>
+	$a = shortcode_atts(array(
+		'icon' => 'heart',
+		'set' => 'fas',
+		'margin' => '',
+		'color' => '',
+		'class' => ''
+	), $atts);
+	$style = '';
+	if (($a['margin'] !== '') && preg_match('/^[ablrty][0-5]$/', $a['margin']) === 1) {
+		// @see <https://getbootstrap.com/docs/4.4/utilities/spacing/>
+		$length = array('0', '.25rem', '.5rem', '1rem', '1.5rem', '3rem')[intval(substr($a['margin'], -1))];
+		switch(substr($a['margin'], 0, 1)) {
+			case 'a':
+				$style .= 'margin:' . $length . ';';
+				break;
+			case 't':
+			case 'y':
+				$style .= 'margin-top:' . $length . ';';
+			case 'b':
+			case 'y':
+				$style .= 'margin-bottom:' . $length . ';';
+				break;
+			case 'l':
+			case 'x':
+				$style .= 'margin-left:' . $length . ';';
+			case 'r':
+			case 'x':
+				$style .= 'margin-right:' . $length . ';';
+		}
+	}
+	if (($a['color'] !== '') && preg_match('/#([a-f0-9]{3}){1,2}\b/i', $a['color']) === 1) {
+		// @see <https://stackoverflow.com/a/12837990>
+		$style .= 'color:' . $a['color'] . ';';
+	}
+	return '<span class="' . $a['set'] . ' fa-' . $a['icon'] . ($a['class'] !== '' ? ' ' . $a['class'] : '') . '"'
+		. ($style !== '' ? ' style="' . $style . '"' : '') . '></span>';
+}
+add_shortcode( 'fa', 'kr8_fontawesome_shortcode' );
+add_filter('nav_menu_item_title', function($title, $item, $args, $depth) {
+	// @see <https://wordpress.stackexchange.com/a/294351>
+	return do_shortcode($title);
+}, 10, 4);
+add_filter('widget_text', 'do_shortcode');
 
 ?>
